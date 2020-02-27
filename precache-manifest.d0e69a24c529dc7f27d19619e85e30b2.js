bfcebc9288c1a67931c8468bd508846f77a5916a
Pavel Kiryanov <p.kiryanov@xsolla.com>
2020-02-27 18:25:17 +0500

    Updates

---
+++ precache-manifest.d0e69a24c529dc7f27d19619e85e30b2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899d4dc02f558f1ab51f0974189c26d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7b7fb5d22dfa9459dcf",
+    "url": "/static/css/2.a3b1ae09.chunk.css"
+  },
+  {
+    "revision": "c85bf88576699a1af439",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b7b7fb5d22dfa9459dcf",
+    "url": "/static/js/2.8a34f27b.chunk.js"
+  },
+  {
+    "revision": "42e429fb0905774ccabbd124742848d4",
+    "url": "/static/js/2.8a34f27b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85bf88576699a1af439",
+    "url": "/static/js/main.453faa5b.chunk.js"
+  },
+  {
+    "revision": "160f7581b244d61475b4",
+    "url": "/static/js/runtime-main.60ef7cc8.js"
+  },
+  {
+    "revision": "c5c1c9cf5d591d6c025d4102a0b33503",
+    "url": "/static/media/alert--orange-transparent.c5c1c9cf.svg"
+  },
+  {
+    "revision": "b529a0c2b87ddb0087525c829c582e87",
+    "url": "/static/media/alert--red-transparent.b529a0c2.svg"
+  },
+  {
+    "revision": "a3d75bc3c2ad42dcfe4472177f9e148f",
+    "url": "/static/media/alert--transparent.a3d75bc3.svg"
+  },
+  {
+    "revision": "2368a18dc9373872e843430eb78146f9",
+    "url": "/static/media/checked--grey.2368a18d.svg"
+  },
+  {
+    "revision": "5400494c34c50565931664b1dba53c3e",
+    "url": "/static/media/checked--red.5400494c.svg"
+  },
+  {
+    "revision": "1e7c71cee64e9d0d642c0b093f4f39e8",
+    "url": "/static/media/checked.1e7c71ce.svg"
+  },
+  {
+    "revision": "648c667a65b90f5c8921a1f78956f8d5",
+    "url": "/static/media/checkmark-in-circle-transparent.648c667a.svg"
+  },
+  {
+    "revision": "adbd790e12bb0b9c7e525a67dc6d4222",
+    "url": "/static/media/close.adbd790e.svg"
+  },
+  {
+    "revision": "57d13acd0e98a391cbb0c4f8416913b4",
+    "url": "/static/media/loader--border.57d13acd.svg"
+  },
+  {
+    "revision": "d926abab7549e4c755871288d5c75984",
+    "url": "/static/media/loader--green.d926abab.svg"
+  },
+  {
+    "revision": "0d6e56652bb8676516f74a5d5e9575b9",
+    "url": "/static/media/quest--blue.0d6e5665.svg"
+  },
+  {
+    "revision": "0228ccc75c5d6cb507728b6fa3171b5a",
+    "url": "/static/media/quest.0228ccc7.svg"
+  },
+  {
+    "revision": "be4100654759facf39893e8eede84b63",
+    "url": "/static/media/separate-list.be410065.svg"
+  },
+  {
+    "revision": "e822c49655db4e28b7e5e9ba13c90ac7",
+    "url": "/static/media/xsolla-pattern.e822c496.svg"
+  }
+]);
